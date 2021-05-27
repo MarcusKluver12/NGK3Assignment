af65da56721c141ed4343e14b5a4e8a6a23e471d
MarcusKluver1 <marcus.kluver@gmail.com>
2021-05-27 11:24:25 +0200

    oprettet til at lave tests

---
+++ NGK3Assignment.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace NGK3Assignment.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+    }
+}
