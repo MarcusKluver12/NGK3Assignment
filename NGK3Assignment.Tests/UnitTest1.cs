fbccce3d120a6ebb682cfbc20581eb2456462290
MikkelWLassen <61686747+MikkelWLassen@users.noreply.github.com>
2021-05-28 19:55:04 +0200

    Unittest

--- NGK3Assignment.Tests/UnitTest1.cs
+++ NGK3Assignment.Tests/UnitTest1.cs
@@ -1,19 +1,87 @@
 using System;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using NGK3Assignment.Data;
+using NGK3Assignment.Models;
 using Xunit;
 
 namespace NGK3Assignment.Tests
 {
-    public class UnitTest1
+    public abstract class TestWithSqlite : IDisposable
+    {
+        private const string InMemoryConnectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _connection;
+
+        protected readonly DbContext DbContext;
+
+        protected TestWithSqlite()
+        {
+            _connection = new SqliteConnection(InMemoryConnectionString);
+            _connection.Open();
+            var options = new DbContextOptionsBuilder<DbContext>()
+                .UseSqlite(_connection)
+                .Options;
+            DbContext = new DbContext(options);
+            DbContext.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+    }
+    public class UnitTest1 : TestWithSqlite
     {
         [Fact]
-        public void Test1()
+        public async Task CanBeConnected()
+        {
+            Assert.True(await DbContext.Database.CanConnectAsync());
+        }
+
+
+        [Fact]
+        public void tableShouldGetCreated()
         {
             //arrange
+            var wether1 = new WeatherStation()
+            {
+                Place = "Herning",
+                Date = DateTime.Today,
+                Celcius = 20,
+                Humidity = 50,
+                Airpressure = 100
+            };
+            var wether2 = new WeatherStation()
+            {
+                Place = "Herning",
+                Date = DateTime.Today,
+                Celcius = 20,
+                Humidity = 50,
+                Airpressure = 100
+            };
+            var wether3 = new WeatherStation()
+            {
+                Place = "Herning",
+                Date = DateTime.Today,
+                Celcius = 20,
+                Humidity = 50,
+                Airpressure = 100
+            };
+
+            DbContext.Add(wether1);
+            DbContext.Add(wether2);
+            DbContext.Add(wether3);
+            DbContext.SaveChanges();
 
             //act
 
             //assert
-
+            Assert.Equal(3, DbContext.Find());
         }
     }
 }
+
